c91b563d930538dc563291f9d368abb487bc601b
Pouya <ppartow1998@gmail.com>
2022-08-12 23:03:01 +0430

    Trying to define a set as union of the sets in a chain which should be its maximal.

--- main.v
+++ main.v
@@ -163,33 +163,115 @@ Definition antisymmetric (U:Type) (R : relation U) : Prop :=
 Definition pao (U:Type) (R: relation U ) : Prop :=
     reflexive U R /\ transitive U R /\ antisymmetric U R.
 
-Definition chain (U:Type) (R: relation U) (S : Ensemble U)  : Prop :=
-    forall x y:U, In U S x -> In U S y -> (R x y \/ R y x).
+(*Taghyir dadam!
+Ye ensemble dge 
+ezafe kardam.*)Definition chain (U:Type) (R: relation U) (S : Ensemble U) (T : Ensemble U)  : Prop :=
+    Included (U) T S /\(forall x y:U, In U T x -> In U T y -> (R x y \/ R y x)).
 
-Definition maximal (U:Type) (x:U) (R:relation U) : Prop:=
-    forall y:U, R x y -> x=y.
+Definition maximal (U:Type) (S:Ensemble U) (x:U) (R:relation U) : Prop:=
+    (forall y:U, In U S y -> R x y -> x=y) /\ In U S x.
 
-Definition chain_sup (U:Type) (R: relation U) (S:Ensemble U) (sup:U): Prop:=
-    chain U R S -> forall x:U, In U S x -> R x sup.
+Definition upper_bound (U:Type) (S:Ensemble U) (R: relation U) (T:Ensemble U) (ub:U): Prop:=
+    (forall x:U, In U T x -> R x ub) /\ In U S ub.
+
+Axiom Zorn's_Lemma: forall (U:Type) (S:Ensemble U) (R:relation U), 
+    pao U R -> (forall (T: Ensemble U), (chain U R S T -> exists (s:U), upper_bound U S R T s) )
+    -> exists (maxim:U), maximal U S maxim R .
 
 
-Axiom Zorn's_Lemma: forall (U:Type) (R:relation U), 
-    pao U R /\ (forall (S: Ensemble U), (chain U R S -> exists (sup:U), chain_sup U R S sup) )
-    -> exists (maxim:U), maximal U maxim R.
+(*Axiom Zorn's_Lemma: forall (U:Type) (S:Ensemble U) (R:relation U), 
+    pao U R -> (forall (T: Ensemble U), (chain U R T -> exists (s:S), sup U R T s) )
+    -> exists (maxim:U), maximal U maxim R.*)
 
+
+(*max va sup ehtemalan niaz be tagheer daran!*)
 
 (*Lemma 1.4.10:*)
-
 Definition Inc : relation (Ensemble Formula) := Included Formula.
 
-Lemma Ex_of_Max0: forall (Gamma : Ensemble Formula), 
+Definition TheSet(Gamma:Ensemble Formula) (Gammap:Ensemble Formula): Prop:=
+    Inc Gamma Gammap /\ Cons Gammap.
+Definition TheS(*name changed from S to TheS*) (Gamma : Ensemble Formula) : Ensemble (Ensemble Formula):= 
+    TheSet Gamma.
+(*Ta ChaharShanbe Ba Alireza Be in Residim!*)
+
+
+
+
+
+
+(*Union of a Chain's Elements:*)
+
+
+Definition PropUoCE (T: Ensemble (Ensemble Formula) ) (Gammap : Ensemble Formula) : Prop :=
+forall (Gamma:Ensemble Formula),( In (Ensemble Formula) T Gamma ->
+forall (f : Formula), (In Formula Gamma f-> In Formula Gammap f )).
+
+Definition PropUoCE2 (T: Ensemble (Ensemble Formula) ) (f : Formula) : Prop :=
+exists (Gamma:Ensemble Formula), (In (Ensemble Formula) T Gamma /\
+In Formula Gamma f).
+
+Definition UoCE1 (T: Ensemble (Ensemble Formula)) : Ensemble Formula :=
+    PropUoCE2 T.
+
+Lemma FuckUnion : forall (T:Ensemble (Ensemble Formula)) (S:Ensemble (Ensemble Formula)),
+chain (Ensemble Formula) Inc S T-> upper_bound (Ensemble Formula) S Inc T (PropUoCE2 T).
+intros T S H. unfold upper_bound. split.
++intros Gamma H1. unfold Inc. unfold Included. intros f H2.
+unfold PropUoCE2.  Admitted. 
+  
+
+Lemma About_PropUoCE : forall (f:Formula) (T:Ensemble (Ensemble Formula)), 
+PropUoCE2 T f -> In Formula (PropUoCE2 T) f.
+Proof. intros f T H. unfold PropUoCE2 in H. Admitted.
+
+
+
+(*Before Here!
+You are strugling!...*)
+
+
+
+
+
+Lemma Ex_of_Max1:forall (Gamma : Ensemble Formula),
+    Cons Gamma -> (exists (Gammap : Ensemble Formula), maximal (Ensemble Formula) (TheS Gamma) Gammap Inc).
+    Proof. intros Gamma H. case Zorn's_Lemma with (U:=Ensemble Formula) (S:=TheSet Gamma) (R:=Inc).
+    +unfold pao. split.
+    -unfold reflexive. intros x. unfold Inc. unfold Included. intros x0 H1. apply H1.
+    -split. 
+    --unfold transitive. intros x y z H1 H2.  unfold Inc in H1. unfold Inc in H2. unfold Inc.
+    unfold Included in H1. unfold Included in H2. unfold Included.
+    intros x0 H3. apply H1 in H3. apply H2 in H3. apply H3.
+    --unfold antisymmetric. intros x y H1 H2. apply Extensionality_Ensembles.
+    unfold Same_set. unfold Inc in H1. unfold Inc in H2. split. ---apply H1. --- apply H2.
+    +intros T H1. exists (PropUoCE2 T) . unfold upper_bound. split.
+    -intros X H2. unfold Inc. unfold Included. intros f.
+        intros H3. apply About_PropUoCE. unfold PropUoCE2.
+        exists X. split. --apply H2. --apply H3.
+    -Admitted.
+    (*+intros x H1. exists x. apply H1.*)
+    
+
+
+
+(*Lemma Ex_of_Max0: forall (Gamma : Ensemble Formula), 
     Cons Gamma -> (exists (Gammap : Ensemble Formula),
-        (maximal (Ensemble Formula) Gammap Inc ) /\ Inc Gamma Gammap).
+    (Cons Gammap) /\ (maximal (Ensemble Formula) Gammap Inc ) /\ Inc Gamma Gammap).
 Proof.
-    intros Gamma H. exists Gamma. split.    
-(*Here!
-You Probably need to change statement of the Lemma, maybe you should break it
-into several Lemmas in order to make it possible to prove.*)
+    intros Gamma H. case Zorn's_Lemma with (U:=Ensemble Formula) (R:=Inc).
+    +unfold pao. split.
+        -unfold reflexive. intros x. unfold Inc. unfold Included. intros x0 H1. apply H1.
+        -split. 
+        --unfold transitive. intros x y z H1 H2.  unfold Inc in H1. unfold Inc in H2. unfold Inc.
+        unfold Included in H1. unfold Included in H2. unfold Included.
+        intros x0 H3. apply H1 in H3. apply H2 in H3. apply H3.
+        --unfold antisymmetric. intros x y H1 H2. apply Extensionality_Ensembles.
+        unfold Same_set. unfold Inc in H1. unfold Inc in H2. split. ---apply H1. --- apply H2.
+    +intros S H1. unfold chain in H1. unfold sup. unfold Inc. admit.
+    +intros x H1. exists x. rename x into Gammap. split. -unfold maximal in H1. admit. 
+        - split. -- apply H1. --unfold maximal in H1. admit.  *)
+
 
 
 
@@ -199,7 +281,7 @@ into several Lemmas in order to make it possible to prove.*)
 
 
 (*Previous attemp for Lemma 1.4.10!:*)
-Definition maxCons (Gamma : Ensemble Formula) : Prop:=
+(*Definition maxCons (Gamma : Ensemble Formula) : Prop:=
     Cons Gamma /\ forall (Gammap : Ensemble Formula), Included Formula Gamma Gammap
         /\ Cons Gammap -> Included Formula Gammap Gamma.
 
@@ -209,16 +291,16 @@ Definition TheSet (Gamma : Ensemble Formula) (X:Ensemble Formula) : Prop :=
 Lemma Ex_of_Max : forall (Gamma:Ensemble Formula),
     Cons Gamma -> exists (Gammap:Ensemble Formula), 
     Included Formula Gamma Gammap /\ maxCons Gammap.
- Admitted.
+ Admitted.*)
 
 (*Function in Lemma 1.4.11:*)
 
-Axiom TheV : nat->bool.
+(*Axiom TheV : nat->bool.
 Axiom VProp : forall (n : nat), TheV n=true <-> In Formula Gamma (atom n)  
 
 
 Definition The (x : nat) : Prop := False.
-
+*)
 (*The Lemma:*)
 Lemma Existence_of_Model: forall (Gamma : Ensemble Formula),
 ~iCons Gamma -> exists (model : nat -> bool), mSsf model Gamma. 
